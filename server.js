f35bd0eb66cf0ba378e8fb3109925441c7a9a039
InConceptio <inconceptio@InConceptiosMBP.fritz.box>
2021-01-15 20:01:51 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 async function main() {
   const favoriteFood = process.env.FAVORITE_FOOD;
-  while(true) {
-    console.log("My favorite food is ${favoriteFood}");
-    await sleep(5000);
-  }
+  console.log(`My favorite food is ${favoriteFood}`);
 }
 
 main();
